5ea171debdef594dc3501b20863547ff5876478f
Hsiaoming Yang <me@lepture.com>
2013-11-11 11:30:36 +0800

    Fix site.js to support data-github

--- site.js
+++ site.js
@@ -4,22 +4,17 @@ function githubCard() {
   var input = d.getElementById('howto-input');
 
   function preview(value) {
-    var bits = value.split('/');
-    if (!bits.length || bits.length > 2) {
+    if (!value) {
       return;
     }
 
     var card = d.createElement('div');
     card.className = 'github-card';
-    card.setAttribute('data-user', bits[0]);
+    card.setAttribute('data-github', value);
 
     var container = d.getElementById('howto-preview');
     while (container.hasChildNodes()) {
       container.removeChild(container.lastChild);
-    }
-
-    if (bits.length == 2) {
-      card.setAttribute('data-repo', bits[1]);
     }
 
     container.appendChild(card);
@@ -36,7 +31,7 @@ function githubCard() {
         location.hash = '#' + value;
       }
       d.documentElement.scrollTop = 630;
-    }
+    };
   }
 
   input.onkeyup = function() {
@@ -44,17 +39,17 @@ function githubCard() {
     timer = setTimeout(function() {
       preview(input.value);
     }, 800);
-  }
+  };
 
   input.onkeydown = function() {
     clearTimeout(timer);
-  }
+  };
 
   if (location.hash) {
     window.onload = function() {
       input.value = location.hash.slice(1);
       preview(input.value);
-    }
+    };
   }
 }
 
