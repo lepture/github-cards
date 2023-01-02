be184c574ba08cd7e4497b716f1e70c9b113856f
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2023-01-02 14:33:11 +0300

    Removed Google Analytics

--- generate.py
+++ generate.py
@@ -5,16 +5,16 @@ import os
 import json
 from subprocess import Popen, PIPE
 
-GA = '''
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', '%s', 'auto');
-var t = qs.user;
-if (qs.repo) t += '/' + qs.repo;
-ga('send', 'pageview', {'title': t});
-'''
+# GA = '''
+# (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+# (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+# m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+# })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+# ga('create', '%s', 'auto');
+# var t = qs.user;
+# if (qs.repo) t += '/' + qs.repo;
+# ga('send', 'pageview', {'title': t});
+# '''
 
 
 def tinyhtml(text):
